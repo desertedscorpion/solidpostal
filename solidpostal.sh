2e820b056dd410c708ce85d855adeea4524adb41
Emory Merryman <emory.merryman@gmail.com>
2016-05-03 00:46:06 +0000

    wait one minute

--- solidpostal.sh
+++ solidpostal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo -e "$(netstat -nr | grep "^0\.0\.0\.0" | awk '{print $2}')\tdockerhost" >> /etc/hosts &&
-    export PATH=${PATH}:/opt/solidpostal
-    for SCRIPT in /usr/local/src/*
+    export PATH=${PATH}:/opt/solidpostal &&
+    sleep 1m &&
+    for SCRIPT in /usr/local/src/*	  
     do
 	/usr/bin/bash ${SCRIPT} &&
 	    true
