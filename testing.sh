df83485e27c7bc249e750ff5500a99025a923ae0
Emory Merryman <emory.merryman@gmail.com>
2016-05-03 13:22:12 +0000

    sleep 30s

--- testing.sh
+++ testing.sh
@@ -12,7 +12,7 @@ docker build -t ninthgrimmercury/solidpostal . &&
 	    true
     fi &&
     docker run --interactive --tty --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --volume ${PWD}/test/src:/usr/local/src:ro -p 127.0.0.1:28860:8080 freakygamma/solidpostal &&
-    sleep 10s &&
+    sleep 30s &&
     if curl http://127.0.0.1:28860 > /dev/null 2>&1
     then
 	echo the web page is up &&
