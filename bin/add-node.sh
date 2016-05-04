da02324e205be5804eaf7da01f137cd05199dcaa
Emory Merryman <emory.merryman@gmail.com>
2016-05-04 16:53:51 +0000

    removed cruft and fixed missing classpath

--- bin/add-node.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat "${@}" | java hudson.cli.CLI -s http://127.0.0.1:8080 create-node $(basename "${@%.*}") &&
-    true
--- bin/install-plugin.sh
+++ bin/install-plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-java hudson.cli.CLI -s http://127.0.0.1:8080 install-plugin "${@}" &&
+export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/local/lib/commons-codec-1.6.jar &&
+    java hudson.cli.CLI -s http://127.0.0.1:8080 install-plugin "${@}" &&
     true
