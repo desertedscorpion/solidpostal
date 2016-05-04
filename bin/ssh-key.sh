8932cfb28bc1d43ce966c9300799a9b5881683e6
Emory Merryman <emory.merryman@gmail.com>
2016-05-04 21:23:53 +0000

    fixed mistake in ssh-key.sh

--- bin/ssh-key.sh
+++ bin/ssh-key.sh
@@ -5,5 +5,5 @@ mkdir --parents /var/lib/jenkins/.ssh &&
     chmod 0700 /var/lib/jenkins/.ssh &&
     cat "${@}" > "/var/lib/jenkins/.ssh/$(basename ${@})" &&
     chown jenkins:jenkins "/var/lib/jenkins/.ssh/$(basename ${@})" &&
-    chmod 0600 jenkins:jenkins "/var/lib/jenkins/.ssh/$(basename ${@})" &&
+    chmod 0600 "/var/lib/jenkins/.ssh/$(basename ${@})" &&
     true
