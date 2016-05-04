7e8dc515f613a1945864caff79bb22996b049116
Emory Merryman <emory.merryman@gmail.com>
2016-05-04 17:06:37 +0000

    adding git.xml

--- test/src/exe.sh
+++ test/src/exe.sh
@@ -3,4 +3,5 @@
 credentials /usr/local/lib/credentials.xml &&
     create-slave /usr/local/lib/slave.xml &&
     create-job /usr/local/lib/job.xml &&
+    create-job /usr/local/lib/git.xml &&
     true
