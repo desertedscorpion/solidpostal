338f68dc5427fd636d52b1786d7f412837549530
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 00:32:41 +0000

    more sleep

--- test/src/exe.sh
+++ test/src/exe.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 credentials /usr/local/lib/credentials.xml &&
+    install-plugin git &&
     ssh-key /var/private/id_rsa &&
     create-slave /usr/local/lib/slave.xml &&
     create-job /usr/local/lib/job.xml &&
     create-job /usr/local/lib/git1.xml &&
     create-job /usr/local/lib/git2.xml &&
-    install-plugin git &&
     dnf install --assumeyes git &&
     export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/local/lib/commons-codec-1.6.jar &&
+    sleep 1m &&
     java hudson.cli.CLI -s http://127.0.0.1:8080 build git1 &&
     java hudson.cli.CLI -s http://127.0.0.1:8080 build git2 &&
     true
--- testing.sh
+++ testing.sh
@@ -12,7 +12,7 @@ docker build -t ninthgrimmercury/solidpostal . &&
 	    true
     fi &&
     docker run --interactive --tty --privileged --detach --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --volume ${PWD}/test/src:/usr/local/src:ro --volume ${HOME}/.private:/var/private -p 127.0.0.1:28860:8080 freakygamma/solidpostal &&
-    sleep 30s &&
+    sleep 5m &&
     if [[ "HTTP/1.1 200 OK" == $(curl --head http://127.0.0.1:28860 | head --lines 1 | tr -d "[:cntrl:]") ]]
     then
 	echo the web page is up &&
