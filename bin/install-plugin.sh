4397ae371f51f71cd22a6ccaf2dc2bfc1902f173
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 01:01:01 +0000

    add a safe-restart and sleep

--- bin/install-plugin.sh
+++ bin/install-plugin.sh
@@ -2,4 +2,6 @@
 
 export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/local/lib/commons-codec-1.6.jar &&
     java hudson.cli.CLI -s http://127.0.0.1:8080 install-plugin "${@}" &&
+    java hudson.cli.CLI -s http://127.0.0.1:8080 safe-restart &&
+    sleep 1m &&
     true
