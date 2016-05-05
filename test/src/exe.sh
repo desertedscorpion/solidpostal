c1131d0b178df6c8aa19aeb46531dac2d57d0e1c
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 10:43:45 +0000

    use job2 that sshes into a slave node, this is better b/c 1 git2 was failing for reasons that I do not care about now; and 2 job2 also verifies that the slave is working

--- test/src/exe.sh
+++ test/src/exe.sh
@@ -6,10 +6,10 @@ credentials /usr/local/lib/credentials.xml &&
     create-slave /usr/local/lib/slave.xml &&
     create-job /usr/local/lib/job.xml &&
     create-job /usr/local/lib/git1.xml &&
-    create-job /usr/local/lib/git2.xml &&
+    create-job /usr/local/lib/job2.xml &&
     dnf install --assumeyes git &&
     export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/local/lib/commons-codec-1.6.jar &&
     sleep 1m &&
     java hudson.cli.CLI -s http://127.0.0.1:8080 build git1 &&
-    java hudson.cli.CLI -s http://127.0.0.1:8080 build git2 &&
+    java hudson.cli.CLI -s http://127.0.0.1:8080 build job2 &&
     true
