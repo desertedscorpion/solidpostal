e4e87c0069714816f122c5a3fa8792e6f7be5159
Emory Merryman <emory.merryman@gmail.com>
2016-05-10 20:49:32 +0000

    use build command

--- test/src/beta.sh
+++ test/src/beta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CLASSPATH=/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar:/usr/share/jenkins/webroot/WEB-INF/remoting.jar:/usr/local/lib/commons-codec-1.6.jar &&
-    dnf install --assumeyes git &&
-    java hudson.cli.CLI -s http://127.0.0.1:8080 build git1 &&
-    java hudson.cli.CLI -s http://127.0.0.1:8080 build job2 &&
+dnf install --assumeyes git &&
+    build git1 &&
+    build job2 &&
+    build job3 &&
     true
